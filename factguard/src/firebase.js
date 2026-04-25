bda9b66fb642015dc505f722c3d13b5714ceee23
MUHAMMAD ANWAR <anwarimdad@gmail.com>
2026-04-25 10:34:51 +0500

    Clear Firebase configuration values
    Removed sensitive Firebase configuration details.

--- factguard/src/firebase.js
+++ factguard/src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC1CGEXISF3oKpNftkPgynbX-iJzvAF46Q",
-  authDomain: "fakenewsauth-3db3d.firebaseapp.com",
-  projectId: "fakenewsauth-3db3d",
-  storageBucket: "fakenewsauth-3db3d.firebasestorage.app",
-  messagingSenderId: "906464698969",
-  appId: "1:906464698969:web:6b12bb116733f8c1b38748",
-  measurementId: "G-MK3BTYXB90",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
